0609044f4508876d251ec05fc6994a0229cafad6
David_B08 <bateson.david@icloud.com>
2026-08-16 00:05:01 +0100

    Update analyse.js

--- pages/api/analyse.js
+++ pages/api/analyse.js
@@ -1,7 +1,10 @@
 import { Redis } from '@upstash/redis';
 import { randomUUID } from 'crypto';
 
-const redis = Redis.fromEnv();
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_KV_REST_API_URL,
+  token: process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN,
+});
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
